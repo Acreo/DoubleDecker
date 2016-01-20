8b742abf855b17ec3f50e967344bd1301302b73d
Pontus.Skoeldstroem <ponsko@acreo.se>
2016-01-20 11:10:38 +0100

    Java client getting closer to working

--- java/src/main/java/se/acreo/doubledecker/DDEvents.java
+++ java/src/main/java/se/acreo/doubledecker/DDEvents.java
@@ -2,8 +2,12 @@ package se.acreo.doubledecker;
 
 public interface DDEvents {
     public void registered(String endpoint);
+
     public void disconnected(String endpoint);
+
     public void publish(String source, String topic, byte[] data);
+
     public void data(String source, byte[] data);
+
     public void error(int code, String reason);
 }
--- java/src/main/java/se/acreo/doubledecker/testDD.java
+++ java/src/main/java/se/acreo/doubledecker/testDD.java
@@ -25,86 +25,117 @@
 */
 package se.acreo.doubledecker;
 
-import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
 import asg.cliche.Command;
 import asg.cliche.Param;
+import asg.cliche.Shell;
 import asg.cliche.ShellFactory;
+
+import java.io.IOException;
+import java.util.*;
 
 
 public class testDD implements Observer, DDEvents {
     DDClient client = null;
-    public testDD(){
+    public testDD() {
     }
-    @Command(description="Connect to a broker")
+
+    public static void main(String[] args) throws IOException {
+        Shell shell = ShellFactory.createConsoleShell("DD", "DoubleDecker java test client", new testDD());
+        shell.commandLoop();
+    }
+
+    @Command(description = "Connect to a broker")
     public void connect(
-            @Param(name="BrokerURL", description="URL the broker is listening on (e.g. tcp://localhost:5555")
+            @Param(name = "BrokerURL", description = "URL the broker is listening on (e.g. tcp://localhost:5555)")
             String brokerUrl,
-            @Param(name="ClientName", description="Name of of the client (e.g. bob)")
-            String name){
+            @Param(name = "ClientName", description = "Name of of the client (e.g. bob)")
+            String name) {
         try {
             client = new DDClient(brokerUrl, name, true, this, "/etc/doubledecker/a-keys.json");
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
-        }
-
-        // we're the observer, subscribe us to the event source
+        }        // we're the observer, subscribe us to the event source
         // can be more observers per client
         // starts the event thread
 
         Thread thread = new Thread(client);
         thread.start();
+
     }
 
-
     @Command(description = "Get status of the DD connection")
-    public String status(){
-        if(client == null){
+    public String status() {
+        if (client == null) {
             return "Not started";
         }
-        if(client.getStatus() == DDClient.CliState.REGISTERED){
+        if (client.getStatus() == DDClient.CliState.REGISTERED) {
             return "Registered";
         } else {
             return "Connecting";
         }
     }
 
+    @Command(description = "List subscriptions and their status")
+    public String subscriptions(){
+        if(client == null){
+            return "Not started";
+        }
+        StringBuilder sb = new StringBuilder();
+        HashMap<List<String>,Boolean> list = client.sublistGet();
+        for (List<String> l : list.keySet()){
+            System.out.println("Subscriptions: " + l);
+            sb.append("Sub: " + l.get(0) + " " + l.get(1)+"\tActive: " + list.get(l) + "\n");
+        }
+        return sb.toString();
+    }
+
     @Command(description = "Send a notifcation to a client")
     public String notify(
-            @Param(name = "Client",description = "Destination of the notification message")
+            @Param(name = "Client", description = "Destination of the notification message")
             String target,
             @Param(name = "Message", description = "The message to send")
-            String message){
-        if(client != null) {
+            String message) {
+        if (client != null) {
             client.sendmsg(target, message);
             return "Sent message!";
         }
         return "Connect first!";
     }
 
-    @Command
-    public String subscribe(String topic, String scope){
-        if(client != null) {
+    @Command(description = "Subscribe to a topic")
+    public String subscribe(
+            @Param(name = "Topic", description = "The topic to subscribe to (place $ at the end for prefix topic)")
+            String topic,
+            @Param(name = "Scope", description = "all/region/cluster/node or /0/2/3/")
+            String scope) {
+        if (client != null) {
             client.subscribe(topic, scope);
             return "Subscribed!";
         }
         return "Connect first!";
     }
 
-    @Command
-    public String unsubscribe(String topic, String scope){
-        if(client != null) {
+    @Command(description = "Unsubscribe from a topic")
+    public String unsubscribe(
+            @Param(name = "Topic", description = "The topic to subscribe to (place $ at the end for prefix topic)")
+            String topic,
+            @Param(name = "Scope", description = "all/region/cluster/node or /0/2/3/")
+            String scope) {
+        if (client != null) {
             client.unsubscribe(topic, scope);
             return "Subscribed!";
         }
         return "Connect first!";
     }
 
-    @Command
-    public String publish(String topic, String message){
-        if(client != null) {
+    @Command(description = "Publish a message to a topic")
+    public String publish(
+            @Param(name = "Topic", description = "The topic to post to")
+            String topic,
+            @Param(name = "Message", description = "The message to post")
+            String message) {
+        if (client != null) {
             client.publish(topic, message);
             return "Published!";
         }
@@ -114,14 +145,9 @@ public class testDD implements Observer, DDEvents {
     // Go for the Observer pattern like this or with the DDEvents approach?
     @Override
     public void update(Observable obj, Object arg) {
-         if (arg instanceof DDMsg) {
+        if (arg instanceof DDMsg) {
             System.out.println("testDD got message: " + arg.toString());
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        ShellFactory.createConsoleShell("hello", "", new testDD())
-                .commandLoop(); // and three.
     }
 
     @Override
@@ -132,21 +158,21 @@ public class testDD implements Observer, DDEvents {
     @Override
     public void disconnected(String endpoint) {
 
-        System.out.println("disconnected: "+ endpoint);
+        System.out.println("Disconnected: " + endpoint);
     }
 
     @Override
     public void publish(String source, String topic, byte[] data) {
-        System.out.println("publish s: " + source + " t: " + topic + " d: " + new String(data));
+        System.out.println("PUBLISH src: " + source + " topic: " + topic + " d: " + new String(data));
     }
 
     @Override
     public void data(String source, byte[] data) {
-        System.out.println("Data s: " + source + " d: " + new String(data));
+        System.out.println("DATA src: " + source + " d: " + new String(data));
     }
 
     @Override
     public void error(int code, String reason) {
-        System.out.println("error c: " + code + " reason: " + reason);
+        System.out.println("ERROR c: " + code + " reason: " + reason);
     }
 }
